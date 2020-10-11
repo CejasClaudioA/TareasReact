8b670a87abae1b6c7d28e8b9233519c9dcd64d2e
Cejas Claudio A <CejasClaudioAlejandro@gmail.com>
2020-10-11 03:40:05 -0300

    add docs/build

---
+++ docs/precache-manifest.8a019d08b2c7d18e91f70d39eaae4330.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f68adce5ba66f3e8c810560bbaa37aab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9830795ebdda7f5b0b9d",
+    "url": "/static/css/main.439b02c9.chunk.css"
+  },
+  {
+    "revision": "46090457346285151c7b",
+    "url": "/static/js/2.f671f749.chunk.js"
+  },
+  {
+    "revision": "4cd4bb82c3efd9af5440b12fb6d92f3f",
+    "url": "/static/js/2.f671f749.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9830795ebdda7f5b0b9d",
+    "url": "/static/js/main.62856868.chunk.js"
+  },
+  {
+    "revision": "ac5e7e61de9a5cfb3a15",
+    "url": "/static/js/runtime-main.db8789ae.js"
+  }
+]);
